fd35f0e390af09e1d46cbf11b6343778b16b731d
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2023-11-25 07:05:29 -0800

    Initial Setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express= require("express");
+require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
 const app = express();
 
@@ -7,9 +8,6 @@ const app = express();
 app.use(express.json()); // Add this middleware to parse JSON data from requests
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/",(req,res)=>{
-res.send("app is working")
-});
 
 //routes
 const authRoute = require('./routes/Auth')
@@ -20,6 +18,9 @@ const userRoute = require('./routes/User')
 app.use('/api/auth',authRoute)
 app.use('/api/user',userRoute)
 
+app.get("/",(req,res)=>{
+res.send("app is working")
+});
 
-const port = 5002
+const port = process.env.PORT
 app.listen(port,console.log(`Listening on port ${port}...`));
