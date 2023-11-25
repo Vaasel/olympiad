03e2ebf15dcb76384ac6e7797d170759f18c605e
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2023-11-25 07:31:12 -0800

    minor backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express= require("express");
 require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
 const app = express();
+const port = process.env.PORT
 
 
 app.use(express.json()); // Add this middleware to parse JSON data from requests
@@ -22,5 +23,4 @@ app.get("/",(req,res)=>{
 res.send("app is working")
 });
 
-const port = process.env.PORT
 app.listen(port,console.log(`Listening on port ${port}...`));
