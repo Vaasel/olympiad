6216d1bab1529e59ede3264e516cdf37f571e93f
Muhammad Haris Azaz Khan <77955039+haris-khan1596@users.noreply.github.com>
2023-12-01 23:10:03 +0500

    Create mailer.js

---
+++ backend/utils/mailer.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.office365.com', // Outlook SMTP server hostname
+  port: 587, // Port for secure TLS connection
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: 'waqasali00123@gmail.com', // Your Outlook email address
+    pass: 'OutlookPassword1#' // Your Outlook email password
+  }
+});
