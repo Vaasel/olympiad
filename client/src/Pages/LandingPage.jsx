e25dd2f95b9f43589b4ce11a2de4b790103cc188
Ubaidullah13 <ubaid.99es@gmail.com>
2023-12-09 13:39:28 +0500

    setup update

--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -1,8 +1,10 @@
+
+
 const LandingPage = () => {
     return (
-        <div className="container" >
-            Insert Landing page Here
-        </div>
+    <div>
+        <h1>Landing Page</h1>
+    </div>
     );
 }
 
