4ac8bae8e82c19ca9cad167a53f23759a92ec5b9
Ubaidullah13 <ubaid.99es@gmail.com>
2023-12-09 23:30:02 +0500

    footer

--- client/src/Pages/LandingPage/LandingPage.jsx
+++ client/src/Pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutUsSection from './AboutUsSection';
 import Hero from './hero/hero';
+import Footer from './footer/footer';
 
 const LandingPage = () => {
   return (
     <div>
       <Hero />
       <AboutUsSection />
+      <Footer />
     </div>
   );
 };
