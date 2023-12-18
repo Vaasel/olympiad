11e91378414eda6475e7095bb6ab8eaea0996bbf
Muhammad Haris Azaz Khan <77955039+haris-khan1596@users.noreply.github.com>
2023-12-19 00:47:11 +0500

    Create SingleUser.jsx

---
+++ client/src/Pages/RegPortal/SingleUser.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import RegLayout from "../../Components/RegLayout";
+import { Grid, Typography } from "@mui/material";
+import axios from "axios";
+import API_URL from "../../config";
+
+const UserDetails = () => {
+  return (
+    <RegLayout>
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{ fontWeight: "bold", fontFamily: "LemonMilkBold" }}
+      >
+        Participant Detail
+      </Typography>
+  </RegLayout>
+  )
+}
